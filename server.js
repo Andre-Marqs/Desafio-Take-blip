8cddfba19bd2baea5ec8a42c65b1cc383a9e8827
Andre-Marqs <69987560+Andre-Marqs@users.noreply.github.com>
2023-05-15 19:00:26 -0300

    server.js

--- server.js
+++ server.js
@@ -2,32 +2,27 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Rota principal
-app.get('/', async (req, res) => {
+// Rota principal para obter os 5 repositórios mais antigos em C#
+app.get('/repositorios', async (req, res) => {
   try {
-    //requisição na API do GitHub
+    // Faz a requisição à API do GitHub
     const response = await axios.get('https://api.github.com/orgs/takenet/repos');
 
-    // Filtra em C#
+    // Filtra os repositórios que são escritos em C#
     const csharpRepos = response.data.filter(repo => repo.language === 'C#');
 
-    // organiza os repositórios mais antigo
+    // Ordena os repositórios pelo mais antigo
     const sortedRepos = csharpRepos.sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
 
-    // Retorna apenas os 5 repositórios mais antigos
+    // Retorna 5 repositórios mais antigos
     const oldestRepos = sortedRepos.slice(0, 5);
 
-    // Retorna a resposta em formato JSON
+    // retorna em JSON
     res.json(oldestRepos);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal server error' });
   }
-});
-
-// Inicia o servidor
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
